527ec515ad220ea7c27b5308f581461cc61a0415
mordovec <kirillstarun@gmail.com>
2026-07-25 19:54:44 +0300

    done 01.1

--- main.rs
+++ main.rs
@@ -6,9 +6,9 @@ fn main() {
     let mut line = String::new();
     stdin.lock().read_line(&mut line).unwrap();
     let nums: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
-    // let shared = Rc::new(nums);
-    // let _a = Rc::clone(&shared);
-    // let _b = Rc::clone(&shared);
-    // println!("count: {}", Rc::strong_count(&shared));
-    // println!("sum: {}", shared.iter().sum::<i32>());
+    let shared = Rc::new(nums);
+    let _a = shared.clone();
+    let _b = shared.clone();
+    println!("count: {}", Rc::strong_count(&shared));
+    println!("sum: {}", shared.iter().sum::<i32>());
 }
