3be64b05b68d9c2f04a2af4d74db2dccac9c6bfa
mordovec <kirillstarun@gmail.com>
2026-07-25 19:39:25 +0300

    start Rust Advanced

---
+++ main.rs
@@ -0,0 +1,14 @@
+use std::io::{self, BufRead};
+use std::rc::Rc;
+
+fn main() {
+    let stdin = io::stdin();
+    let mut line = String::new();
+    stdin.lock().read_line(&mut line).unwrap();
+    let nums: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
+    // let shared = Rc::new(nums);
+    // let _a = Rc::clone(&shared);
+    // let _b = Rc::clone(&shared);
+    // println!("count: {}", Rc::strong_count(&shared));
+    // println!("sum: {}", shared.iter().sum::<i32>());
+}
