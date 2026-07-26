d5a6cdadbc0ad51f4eb07b1ad7365c81f7834865
mordovec <kirillstarun@gmail.com>
2026-07-26 04:55:17 +0300

    done 03.3

--- main.rs
+++ main.rs
@@ -1,28 +1,8 @@
-trait Shape { fn area(&self) -> f64; }
-struct Square {
-    side: f64,
-}
-struct Triangle {
-    base: f64,
-    height: f64,
-}
-impl Shape for Square {
-    fn area(&self) -> f64 {
-        self.side * self.side
-    }
-}
-impl Shape for Triangle {
-    fn area(&self) -> f64 {
-        0.5 * self.base * self.height
-    }
+async fn double(n: i32) -> i32 {
+    n * 2
 }
 
 fn main() {
-    let shapes: Vec<Box<dyn Shape>> = vec![
-        Box::new(Square { side: 3.0 }),
-        Box::new(Triangle { base: 4.0, height: 5.0 }),
-    ];
-    for s in &shapes {
-        println!("{:.2}", s.area());
-    }
+    let _fut = double(7);
+    println!("created future");
 }
